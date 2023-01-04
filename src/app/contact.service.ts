645361a06ace30d2fd926fb82c4593c14208efbf
baruch yakubov <baruchyaku@gmail.com>
2023-01-04 16:43:07 +0200

    added favicon

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,116 +8,116 @@ const CONTACTS = [
         "_id": "5a56640269f443a5d64b32ca",
         "name": "Ochoa Hyde",
         "email": "ochoahyde@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a5664025f6ae9aa24a99fde",
         "name": "Hallie Mclean",
         "email": "halliemclean@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a56640252d6acddd183d319",
         "name": "Parsons Norris",
         "email": "parsonsnorris@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a566402ed1cf349f0b47b4d",
         "name": "Rachel Lowe",
         "email": "rachellowe@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a566402abce24c6bfe4699d",
         "name": "Dominique Soto",
         "email": "dominiquesoto@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a566402a6499c1d4da9220a",
         "name": "Shana Pope",
         "email": "shanapope@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a566402f90ae30e97f990db",
         "name": "Faulkner Flores",
         "email": "faulknerflores@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a5664027bae84ef280ffbdf",
         "name": "Holder Bean",
         "email": "holderbean@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a566402e3b846c5f6aec652",
         "name": "Rosanne Shelton",
         "email": "rosanneshelton@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a56640272c7dcdf59c3d411",
         "name": "Pamela Nolan",
         "email": "pamelanolan@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a5664029a8dd82a6178b15f",
         "name": "Roy Cantu",
         "email": "roycantu@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a5664028c096d08eeb13a8a",
         "name": "Ollie Christian",
         "email": "olliechristian@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a5664026c53582bb9ebe9d1",
         "name": "Nguyen Walls",
         "email": "nguyenwalls@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a56640298ab77236845b82b",
 
         "name": "Glenna Santana",
         "email": "glennasantana@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a56640208fba3e8ecb97305",
         "name": "Malone Clark",
         "email": "maloneclark@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a566402abb3146207bc4ec5",
         "name": "Floyd Rutledge",
         "email": "floydrutledge@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a56640298500fead8cb1ee5",
         "name": "Grace James",
         "email": "gracejames@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a56640243427b8f8445231e",
         "name": "Tanner Gates",
         "email": "tannergates@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     },
     {
         "_id": "5a5664025c3abdad6f5e098c",
         "name": "Lilly Conner",
         "email": "lillyconner@renovize.com",
-        "phone": "054=65566776"
+        "phone": "054-65566776"
     }
 ];
 
